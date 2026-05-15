b10fd7acc8b955470df9a6bf12909d2192145230
shanmukha <jakka.shanmukha2003@gmail.com>
2026-05-15 11:16:54 +0530

    added function in js

--- day-7/script.js
+++ day-7/script.js
@@ -1,9 +1,27 @@
-console.log(myfun(1,1))
+// console.log(myfun(1,1))
 
-function myfun(num1,num2){
-    return num1 - num2
-}
+// function myfun(num1,num2){
+//     return num1 - num2
+// }
 
-function myfun(num1,num2){
-    return num1+num2
-}
+// function myfun(num1,num2){
+//     return num1+num2
+// }
+
+// function myfun(){    //----> function declaration
+
+// }
+
+
+// const fun=function(num1,num2){
+//     return num1-num2                 //--->  function expression
+// }
+
+// const fun=(num1,num2)=>{
+//     return num1+num2                //--->arrow function
+// }                
+
+
+const fun =(num1,num2)=>num1+num2    //----> bullet function
+
+console.log(fun(1,1));
